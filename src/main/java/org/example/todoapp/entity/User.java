9013edc286504b7be63dd61c2c8d819311f9ce9d
hwan <hahaha1719@naver.com>
2024-06-11 10:28:38 +0900

    todoapp Commit

---
+++ src/main/java/org/example/todoapp/entity/User.java
@@ -0,0 +1,17 @@
+package org.example.todoapp.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+    public String name;
+    public String email;
+}
